134bd6838bef68803ea17b8d4c4cc6854cd5146a
tjackr <tjackr@proton.me>
2026-09-06 20:27:20 +0200

    Removed old renamings and updated main CMakeLists

--- main/esp_miser.cpp
+++ main/esp_miser.cpp
@@ -1,3 +1,5 @@
+#include "esp_err.h"
+#include "freertos/projdefs.h"
 #include "http_client.hpp"
 #include "wifi.hpp"
 
@@ -7,7 +9,6 @@
 #include <string>
 #include <nvs_flash.h>
 #include <esp_event.h>
-
 
 
 static const char* TAG = "esp_miser";
@@ -21,38 +22,56 @@ void test_http_task_oneshot(void* param) {
 
   // Create client
   EspHttpClient client(Cfg);
+  while (true)
+  {
+    // --- Request 1: GET ---
+    esp_err_t err = client.get("http://httpforever.com");
+    if (err == ESP_OK) {
+      int status = client.check_status_code();
+      std::string body;
+      client.read_body(body);
+      ESP_LOGI("http_task", "GET Status: %d, Body: %s", status, body.c_str());
+    }
 
-  // --- Request 1: GET ---
-  esp_err_t err = client.get("http://duckduckgo.com");
-  if (err == ESP_OK) {
-    int status = client.check_status_code();
-    std::string body;
-    client.read_body(body);
-    ESP_LOGI("TASK", "GET Status: %d, Body: %s", status, body.c_str());
+    // --- Request 2: POST (Reusing same client) ---
+    // Note: With the reset_request_state fix, headers from GET won't leak here.
+    // std::string payload = "{\"key\": \"value\"}";
+    // err = client.post("http://api.example.com/update", payload);
+    // if (err == ESP_OK) {
+    //   int status = client.check_status_code();
+    //   ESP_LOGI("TASK", "POST Status: %d", status);
+    // }
+
+    // Destructor runs client_clean() automatically
+    // vTaskDelete(NULL);
+    vTaskDelay(pdMS_TO_TICKS(5000));
   }
-
-  // --- Request 2: POST (Reusing same client) ---
-  // Note: With the reset_request_state fix, headers from GET won't leak here.
-  // std::string payload = "{\"key\": \"value\"}";
-  // err = client.post("http://api.example.com/update", payload);
-  // if (err == ESP_OK) {
-  //   int status = client.check_status_code();
-  //   ESP_LOGI("TASK", "POST Status: %d", status);
-  // }
-
-  // Destructor runs client_clean() automatically
-  vTaskDelete(NULL);
 }
 
 
 
 extern "C" void app_main(void) {
+  esp_err_t res;
+
   ESP_ERROR_CHECK(nvs_flash_init());
   ESP_ERROR_CHECK(esp_netif_init());
   ESP_ERROR_CHECK(esp_event_loop_create_default());
+  esp_netif_create_default_wifi_sta(); // This line is the result of many headbashes
 
-  ESP_ERROR_CHECK(g_wifihandler.wifi_init());
-  ESP_ERROR_CHECK(g_wifihandler.wifi_connect());
+  res = g_wifihandler.wifi_init();
+  ESP_ERROR_CHECK(res);
+  if (res == ESP_FAIL)
+  {
+    ESP_LOGE(TAG, "Failed to init wifi, res: %s", esp_err_to_name(res)); 
+  }
+
+  // Blocks until we have an IP or die
+  res = g_wifihandler.wifi_wait_for_connect();
+  ESP_ERROR_CHECK(res);
+  if (res != ESP_OK)
+  {
+    ESP_LOGE(TAG, "Failed to connect wifi try again, res: %s", esp_err_to_name(res));
+  }
 
   ESP_LOGI(TAG, "IP: %s", g_wifihandler.get_ip().c_str());
 
